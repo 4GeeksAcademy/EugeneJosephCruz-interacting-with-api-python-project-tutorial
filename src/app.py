c44c9cba9aa62e40585c609cf27a3bec5b32e94e
Eugene Cruz <126279186+EugeneJosephCruz@users.noreply.github.com>
2023-12-21 00:34:16 +0000

    completed project.

--- src/app.py
+++ src/app.py
@@ -1,15 +1,68 @@
+import spotipy
 import os
-from sqlalchemy import create_engine
-import pandas as pd
 from dotenv import load_dotenv
+from spotipy.oauth2 import SpotifyClientCredentials
+import pandas as pd
+import matplotlib.pyplot as plt
 
-# load the .env file variables
+
 load_dotenv()
 
-# 1) Connect to the database here using the SQLAlchemy's create_engine function
 
-# 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
 
-# 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
 
-# 4) Use pandas to print one of the tables as dataframes using read_sql function
+CLIENT_ID = os.environ.get("CLIENT_ID")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+
+spotipy.Spotify()
+
+artist_id = "7HGNYPmbDrMkylWqeFCOIQ"
+
+client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+top_tracks = sp.artist_top_tracks(artist_id)
+print(top_tracks)
+
+for track in top_tracks['tracks'][:10]:
+    print(track['name'])
+    print(track['popularity'])
+    print(track['duration_ms'])
+
+import pandas as pd
+
+# Your dictionary
+data = {
+    'tracks': [
+        # ... (your track data)
+    ]
+}
+
+# Extracting track name, popularity, and duration
+track_data = []
+for track in top_tracks['tracks'][:10]:
+    name = track['name']
+    popularity = track['popularity']
+    duration_ms = track['duration_ms']
+    track_data.append({'name': name, 'popularity': popularity, 'duration_ms': duration_ms})
+
+# Creating DataFrame
+df = pd.DataFrame(track_data)
+
+# Displaying the DataFrame
+print(df)
+
+
+
+plt.figure(figsize=(10, 6))
+plt.scatter(df["duration_ms"], df["popularity"], c=df.index, cmap="Set1")
+plt.title("Relationship Between Song Duration and Popularity")
+plt.xlabel("Duration (milliseconds)")
+plt.ylabel("Popularity")
+plt.colorbar(label="Track Index")
+
+plt.tight_layout()
+plt.savefig("scatterplot.png")
+plt.show()
+
+print("After taking a look at the scatterplot, I see no relation between duration and popularity.")
